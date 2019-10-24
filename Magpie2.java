20a81d5d98ca1df327633e45ddad55f2947f5d8c
aarush3002 <aarush3002@gmail.com>
2019-10-24 10:48:04 -0700

    continuing to change to find keyword

--- Magpie2.java
+++ Magpie2.java
@@ -28,6 +28,7 @@ public class Magpie2
      *            the user statement
      * @return a response based on the rules given
      */
+    //part c edits start here
     public String getResponse(String statement)
     {
         String response = "";
@@ -35,20 +36,20 @@ public class Magpie2
         if (statement.trim().length() < 1)
         {
             response = "Say something, please.";
-        } else if (statement.indexOf("no") >= 0)
+        } else if (findKeyword(statement ,"no", 0) >= 0)
         {
             response = "Why so negative?";
         }
-        else if (statement.indexOf("mother") >= 0
-                || statement.indexOf("father") >= 0
-                || statement.indexOf("sister") >= 0
-                || statement.indexOf("brother") >= 0)
+        else if (findKeyword(statement ,"mother", 0) >= 0
+                || findKeyword(statement ,"father", 0) >= 0
+                || findKeyword(statement ,"sister", 0) >= 0
+                || findKeyword(statement ,"brother", 0) >= 0)
         {
             response = "Tell me more about your family.";
         }
         //Activity 2, #1
-        else if (statement.indexOf("dog") >= 0
-                || statement.indexOf("cat") >= 0)
+        else if (findKeyword(statement ,"dog", 0) >= 0
+                || findKeyword(statement ,"cat", 0) >= 0
         {
             response = "Tell me more about your pets.";
         }
@@ -82,7 +83,78 @@ public class Magpie2
         }
         return response;
     }
+    //part c edits end here	
+    private int findKeyword(String statement, String goal,
+    int startPos)
+    {
+        String phrase = statement.trim();
+        // The only change to incorporate the startPos is in
+        // the line below
+        int psn = phrase.toLowerCase().indexOf(
+                goal.toLowerCase(), startPos);
 
+        // Refinement--make sure the goal isn't part of a
+        // word
+        int i = 1;
+        while (psn >= 0)
+        {
+            // Find the string of length 1 before and after
+            // the word
+            String before = " ", after = " ";
+            if (psn > 0)
+            {
+                before = phrase.substring(psn - 1, psn)
+                .toLowerCase();
+            }
+            if (psn + goal.length() < phrase.length())
+            {
+                after = phrase.substring(
+                    psn + goal.length(),
+                    psn + goal.length() + 1)
+                .toLowerCase();
+            }
+            System.out.println( i + ", " + psn + ", " + before + ", " + after);
+            // If before and after aren't letters, we've
+            // found the word
+            if (((before.compareTo("a") < 0) || (before
+                    .compareTo("z") > 0)) // before is not a
+                // letter
+            && ((after.compareTo("a") < 0) || (after
+                    .compareTo("z") > 0)))
+            {
+                System.out.println( i + ", " + psn + ", " + before + ", " + after);
+                return psn;
+            }
+
+            // The last position didn't work, so let's find
+            // the next, if there is one.
+            psn = phrase.indexOf(goal.toLowerCase(),
+                psn + 1);
+            i += 1;
+            System.out.println( i + ", " + psn + ", " + before + ", " + after);
+        }
+
+        return -1;
+    }
+    // PLTW 1.1.6 Part C end
+    /**
+     * Search for one word in phrase. The search is not case
+     * sensitive. This method will check that the given goal
+     * is not a substring of a longer string (so, for
+     * example, "I know" does not contain "no"). The search
+     * begins at the beginning of the string.
+     * 
+     * @param statement
+     *            the string to search
+     * @param goal
+     *            the string to search for
+     * @return the index of the first occurrence of goal in
+     *         statement or -1 if it's not found
+     */
+    private int findKeyword(String statement, String goal)
+    {
+        return findKeyword(statement, goal, 0);
+    }
     /**
      * Pick a default response to use if nothing else fits.
      * @return a non-committal string
